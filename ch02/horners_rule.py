6d75aef88f5f84bb16da2a1a740eb22dff957eaa
Robert Scott Dionne <robertsdionne@gmail.com>
2014-07-16 09:52:27 -0400

    Add horner's rule.

---
+++ ch02/horners_rule.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import argparse
+
+
+def horners_rule(items, x):
+  y = 0
+  for i in xrange(0, len(items)):
+    y = items[i] + x * y
+  return y
+
+
+def main():
+  commands = argparse.ArgumentParser(description = 'Horner\'s rule.')
+  commands.add_argument(
+      'integers', metavar = 'N', type = float, nargs = '+', help = 'Polynomial coefficients.')
+  commands.add_argument(
+      '--value', metavar = 'x', type = float, help = 'Value of x.')
+  arguments = commands.parse_args()
+  print horners_rule(arguments.integers, arguments.value)
+
+
+if '__main__' == __name__:
+  main()
